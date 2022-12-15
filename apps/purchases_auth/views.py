d095e2823326698e4d3c5c70a2a2c82604f02bb2
axxwel <alexandregimeno@gamoap.com>
2022-12-15 12:34:21 +0100

    init templates, statics

--- apps/purchases_auth/views.py
+++ apps/purchases_auth/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from purchases_auth.models import Datas
+
+def hello(request):
+    datas = Datas.objects.all()
+    return render(request,
+    'purchases_auth/hello.html',
+    {'datas': datas})
+
+def about(request):
+    return HttpResponse('<h1>About apps</h1>')
