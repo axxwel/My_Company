c2fb3a282eb37d89e93208b0d83453a0f46e81fe
axxwel <alexandregimeno@gamoap.com>
2022-12-19 16:17:52 +0100

    check tables models errors

--- apps/purchases_auth/admin.py
+++ apps/purchases_auth/admin.py
@@ -1,34 +1,8 @@
 from django.contrib import admin
 
-from purchases_auth.models import Payment_method
-from purchases_auth.models import Purchase_type
-from purchases_auth.models import Branch
-from purchases_auth.models import Process
-from purchases_auth.models import Threshold
 from purchases_auth.models import Order
-
-
-class Payment_method_admin(admin.ModelAdmin):
-    list_display = ('name')
-
-class Purchase_type_admin(admin.ModelAdmin):
-    list_display = ('name')
-
-class Branch_admin(admin.ModelAdmin):
-    list_display = ('name', 'controler_login')
-    
-class Process_admin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'date', 'asker_login', 'controler_login', 'controler_auth')
-    
-class Threshold_admin(admin.ModelAdmin):
-    list_display = ('name', 'threshold_1', 'threshold_2', 'threshold_3', 'controler', 'controler_auth')
     
 class Order_admin(admin.ModelAdmin):
-    list_display = ('order_id', 'date', 'asker_login', 'controler_login', 'controler_auth')
+    list_display = ('order_id', 'date', 'controler_auth')
 
-admin.site.register(Payment_method, Payment_method_admin)
-admin.site.register(Purchase_type, Purchase_type_admin)
-admin.site.register(Branch, Branch_admin)
-admin.site.register(Process, Process_admin)
-admin.site.register(Threshold, Threshold_admin)
 admin.site.register(Order, Order_admin)
--- apps/purchases_auth/models.py
+++ apps/purchases_auth/models.py
@@ -16,14 +16,14 @@ class Branch(models.Model):
     def __str__(self):
         return f'{self.name}'
     name = models.fields.CharField(max_length=30, unique=True)
-    #controler_login = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    controler_login = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
 class Process(models.Model):
     def __str__(self):
         return f'{self.name}'
     name = models.fields.CharField(max_length=30, unique=True)
-    #Branch = models.ForeignKey(Branch, null=True, on_delete=models.SET_NULL)
-    #controler_login = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    Branch = models.ForeignKey(Branch, null=True, on_delete=models.SET_NULL)
+    controler_login = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
 class Threshold(models.Model):
     def __str__(self):
@@ -33,7 +33,7 @@ class Threshold(models.Model):
     threshold_2 = models.fields.IntegerField()
     threshold_3 = models.fields.IntegerField()
 
-    #controler = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    controler = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     #super_controler = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
 class Order(models.Model):
@@ -48,13 +48,13 @@ class Order(models.Model):
     order_id = models.fields.CharField(max_length=17, unique=True)
     date = models.fields.DateTimeField(auto_now=True)
 
-    #asker_login = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    asker_login = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     
-    #purchase_type = models.ForeignKey(Purchase_type, null=True, on_delete=models.SET_NULL)
-    #branch = models.ForeignKey(Branch, null=True, on_delete=models.SET_NULL)
-    #process = models.ForeignKey(Process, null=True, on_delete=models.SET_NULL)
+    purchase_type = models.ForeignKey(Purchase_type, null=True, on_delete=models.SET_NULL)
+    branch = models.ForeignKey(Branch, null=True, on_delete=models.SET_NULL)
+    process = models.ForeignKey(Process, null=True, on_delete=models.SET_NULL)
 
-    #payment_method = models.ForeignKey(Payment_method, null=True, on_delete=models.SET_NULL)
+    payment_method = models.ForeignKey(Payment_method, null=True, on_delete=models.SET_NULL)
 
     product = models.fields.CharField(max_length=17, unique=True)
     price = models.fields.IntegerField()
@@ -65,7 +65,7 @@ class Order(models.Model):
     unit_price = models.fields.IntegerField()
     delivery_date = models.fields.DateTimeField()
 
-    #controler_login = models.ForeignKey(Branch, null=True, on_delete=models.SET_NULL)
+    #controler_login = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     controler_auth = models.fields.CharField(choices=auth_status.choices, default=auth_status.PENDING, max_length=10)
     controler_comment = models.fields.CharField(max_length=254, null=True)
